7c7cca7c5f8025c3a263c04c454b8d1a6adea33d
Rluansilva <47661324+Rluansilva@users.noreply.github.com>
2019-02-18 09:20:58 -0300

    Add files via upload

---
+++ exerciciocpf.py
@@ -0,0 +1,19 @@
+cpf=1234
+print (cpf)
+print ('o meu cpf e',cpf)
+print (type(cpf))
+
+Nome= "Roger"
+Tel= 40028922
+cpf= 1234
+altura= 1.80
+
+print ('seu nome é', Nome)
+print ('seu telefone é', Tel)
+print ('seu cpf é',cpf)
+print ('sua altura é',altura)
+
+print (type(Nome))
+print (type (Tel))
+print (type (cpf))
+print (type (altura))
